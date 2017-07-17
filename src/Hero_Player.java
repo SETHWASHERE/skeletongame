ee669f7e859105e609f1f5db1fa6f9e683a5e87d
SETHWASHERE <sethtanriedy@gmail.com>
2017-07-17 15:45:35 -0700

    Added skeleton case data, as well as hero case data. Made some changed and addition to the code

--- src/Hero_Player.java
+++ src/Hero_Player.java
@@ -1,100 +1,76 @@
-import java.util.Scanner;
+import edu.digipen.gameobject.GameObject;
+
+import java.awt.event.KeyEvent;
 
 /**
  * Created by seth.riedy on 7/17/2017.
  */
-public class Main {
-    public static int main(String[] args) {
 
-        int exp = 0;
-        int herolevel = 1;
-        int heroattack = 7;
-        int herodefense = 2;
-        int herohp = 10;
+    public class Hero_Player extends GameObject {
+    int exp = 0;
+    int herolevel = 1;
+    int heroattack = 7;
+    int herodefense = 2;
+    int herohp = 10;
+    boolean turn = true;
 
-        int skeletonattack = 4;
-        int skeletonenemyhp = 20;
+    public Hero_Player() {
+        super("Hero_player", 64, 64, "");
+    }
 
-        boolean turn = false;
-        while (skeletonenemyhp <= 0);
-        {
-            herohp -= skeletonattack;
-            boolean turn1 = true;
+    public void update() {
 
-            if (turn1)
-            {
-                skeletonenemyhp -= heroattack;
-            }
-        }
 
-        if (skeletonenemyhp == 0)
-        {
-            System.out.println("enemy defeated!");
-            exp += 10;
-        }
-
-        //hero level design 1-11(stat increases, etc.)
-        while (herolevel != 11)
-        {
+        while (herolevel != 11) {
             if (exp == 10 || exp == 30 || exp == 60 || exp == 100 || exp == 150 || exp == 210 || exp == 280 || exp == 360 || exp == 450 || exp == 1000) {
                 herolevel += 1;
 
-                if (herolevel == 2)
-                {
+                if (herolevel == 2) {
                     herohp += 2;
                     heroattack += 2;
                     herodefense += 2;
 
-                    if (herolevel == 3)
-                    {
+                    if (herolevel == 3) {
                         herohp += 3;
                         heroattack += 5;
                         herodefense += 2;
 
-                        if (herolevel == 4)
-                        {
+                        if (herolevel == 4) {
                             herohp += 5;
                             heroattack += 6;
                             herodefense += 2;
 
-                            if (herolevel == 5)
-                            {
+                            if (herolevel == 5) {
                                 herohp += 5;
                                 heroattack += 10;
                                 herodefense += 2;
 
-                                if (herolevel == 6)
-                                {
+                                if (herolevel == 6) {
                                     herohp += 5;
                                     heroattack += 10;
                                     herodefense += 3;
 
-                                    if (herolevel == 7)
-                                    {
+                                    if (herolevel == 7) {
                                         herohp += 7;
                                         heroattack += 10;
                                         herodefense += 3;
 
-                                        if (herolevel == 8)
-                                        {
+                                        if (herolevel == 8) {
                                             herohp += 7;
                                             heroattack += 20;
                                             herodefense += 2;
 
-                                            if (herolevel == 9)
-                                            {
+                                            if (herolevel == 9) {
                                                 herohp += 9;
                                                 heroattack += 20;
                                                 herodefense += 2;
 
-                                                if (herolevel == 10)
-                                                {
+                                                if (herolevel == 10) {
                                                     herohp += 5;
                                                     heroattack += 30;
                                                     herodefense += 3;
 
-                                                    if (herolevel == 11)
-                                                    {
+                                                    if (herolevel == 11) {
                                                         herohp += 999;
                                                         heroattack += 999;
                                                         herodefense += 999;
@@ -109,7 +85,8 @@ public class Main {
                     }
                 }
             }
-            return 0;
         }
+
     }
 }
+
---
+++ src/skeleton.java
@@ -0,0 +1,26 @@
+import edu.digipen.gameobject.GameObject;
+
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by seth.riedy on 7/17/2017.
+ */
+public class skeleton extends GameObject
+{
+    int skeletonattack = 4;
+    int skeletonenemyhp = 20;
+    boolean turn = true;
+
+    public skeleton()
+    {
+        super("skeleton", 64, 64, "");
+    }
+
+    public void update()
+    {
+        if (skeletonenemyhp > 0)
+        {
+            while ()
+        }
+    }
+}
